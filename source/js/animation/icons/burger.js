98fb8bec0474b41f5915f18916c4390b401e1df4
EugeneVeremchuk <everemchuk1978@gmail.com>
2022-10-08 11:53:03 -0700

    burger

--- source/js/animation/icons/burger.js
+++ source/js/animation/icons/burger.js
@@ -25,29 +25,30 @@ function burgerAnimation(event) {
 
   function animationOpen() {
     const tlOpen = gsap.timeline({ onStart: lock, onComplete: unlock })
-    tlOpen.to("#top", { duration: .1, ease: "power4.out", y: 105 })
-    tlOpen.to("#bottom", { y: -5, transformOrigin: "50% 50%", scale: 0, autoAlpha: 0 }, 0)
-    tlOpen.to("#top", { duration: .1, ease: "power4.out", y: -100 })
-    tlOpen.to('#cross-1', { duration: 1, ease: "bounce.out", y: -30, rotate: 50, transformOrigin: "50% 50%" }, "-=50%")
-    tlOpen.to('#cross-2', { duration: 1, ease: "bounce.out", y: -30, rotate: -50, transformOrigin: "50% 50%" }, "<")
+    tlOpen.to("#top", { duration: .2, ease: "power1.inOut", y: 55 })
+    tlOpen.to("#bottom", { duration: .2, y: -55, }, "<")
+    tlOpen.to("#bottom", { duration: .1, ease: "power1.inOut", transformOrigin: "50% 50%", scale: 0, autoAlpha: 0 })
+    tlOpen.to("#top", { duration: .2, ease: "power1.inOut", y: -100 })
+    tlOpen.to('#middle1', { duration: 1, ease: "bounce.out", y: -30, rotate: 50, transformOrigin: "50% 50%" }, "-=20%")
+    tlOpen.to('#middle2', { duration: 1, ease: "bounce.out", y: -30, rotate: -50, transformOrigin: "50% 50%" }, "<")
     return tlOpen
   }
 
   function animationClose() {
     const tlClose = gsap.timeline({ onStart: lock, onComplete: unlock })
-    tlClose.to("#top", { duration: .1, ease: "power4.out", y: 105 })
-    tlClose.to("#bottom", { duration: .1, y: -5, transformOrigin: "50% 50%", scale: 1, autoAlpha: 1 }, 0)
-    tlClose.to('#cross-1', { duration: 0, ease: "bounce.out", y: 0, rotate: 0, transformOrigin: "50% 50%" })
-    tlClose.to('#cross-2', { duration: 0, ease: "bounce.out", y: 0, rotate: 0, transformOrigin: "50% 50%" })
-    tlClose.to("#top", { duration: .1, ease: "elastic.out(1, 0.4)", y: 0 })
-    tlClose.to("#bottom", { duration: 0, y: 0, }, "<")
+    tlClose.to('#middle1', { duration: 1, ease: "bounce.out", y: 0, rotate: 0, transformOrigin: "50% 50%" })
+    tlClose.to('#middle2', { duration: 1, ease: "bounce.out", y: 0, rotate: 0, transformOrigin: "50% 50%" }, "<")
+    tlClose.to("#top", { duration: .2, ease: "power1.inOut", y: 55 }, "<")
+    tlClose.to("#bottom", { duration: .1, ease: "power1.inOut", transformOrigin: "50% 50%", scale: 1, autoAlpha: 1 })
+    tlClose.to("#top", { duration: .2, ease: "power1.inOut", y: 0 })
+    tlClose.to("#bottom", { duration: .2, y: 0, }, "-=70%")
     return tlClose
   }
 
   switch (condition) {
     case 'open':
       burger.classList.remove('open')
-      setTimeout(() => burger.classList.add('close'), 200)
+      setTimeout(() => burger.classList.add('close'), 1000)
       condition = 'close'
       animationOpen()
       break
@@ -55,7 +56,7 @@ function burgerAnimation(event) {
       setTimeout(() => {
         burger.classList.remove('close')
         burger.classList.add('open')
-      }, 100)
+      }, 1000)
       condition = 'open'
       animationClose()
       break
