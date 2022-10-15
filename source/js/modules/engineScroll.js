ae2876d6638c493954b6d727493eb63a9e8a1ebc
EugeneVeremchuk <everemchuk1978@gmail.com>
2022-10-15 00:18:58 -0700

    laptop test

--- source/js/modules/engineScroll.js
+++ source/js/modules/engineScroll.js
@@ -3,26 +3,30 @@ let zSpacing = -1000
 let zValues = []
 let lastPosition = zSpacing / 5
 const $frames = document.querySelectorAll('.frame')
-const frames = Array.from($frames)
-
 
 function engineScroll(event) {
   
   let top = document.documentElement.scrollTop
+  console.log('top', top)
   let delta = lastPosition - top
+  console.log('delta', delta)
   lastPosition = top
+  console.log('lastPos', lastPosition)
   
-  frames.forEach((element, index) => {
-    zValues.push((index * zSpacing))
-    zValues[index] += delta * -3
-    let frame = frames[index]
+  $frames.forEach((frame, index) => {
+    zValues.push((index * zSpacing) + zSpacing)
+    zValues[index] += delta * -5.5
     let transfrom = `translateZ(${zValues[index]}px)`
-    let opacity = zValues[index] < Math.abs(zSpacing) / 1.5 ? 1 : 0
+    let opacity = zValues[index] < Math.abs(zSpacing) / 1.8 ? 1 : 0
     frame.setAttribute('style', `transform: ${transfrom}; opacity: ${opacity}`)
   })
 
 }
 
-window.addEventListener('scroll', engineScroll)
+window.addEventListener('scroll', function(event) {
+  console.log(document.documentElement.scrollTop)
+})
 
-window.scrollTo(0, 1)
+//window.scrollTo(0, 10)
+
+
